baaa9e54af1edec7451aebb2cc3aa4ef8483c8bd
alek101 <47416711+alek101@users.noreply.github.com>
2022-02-15 15:50:32 +0100

    end

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ import {
     getDoc,
     updateDoc
 } from 'firebase/firestore'
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword, signOut,
+    onAuthStateChanged
+} from 'firebase/auth'
 
 const firebaseConfig = {
     apiKey: "AIzaSyBC0oFHvw3HTyKg5pA8WZ0g0-uxGLzrXho",
@@ -23,6 +29,7 @@ initializeApp(firebaseConfig)
 
 //init servise
 const db = getFirestore()
+const auth = getAuth()
 
 // collection ref
 
@@ -110,4 +117,53 @@ onSnapshot(q, (snapshot) => {
       .then(() => {
           updateForm.reset()
       })
+  })
+
+  // signing users up
+const signupForm = document.querySelector('.signup')
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = signupForm.email.value
+  const password = signupForm.password.value
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user created:', cred.user)
+      signupForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+const loginForm = document.querySelector('.login')
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const email = loginForm.email.value
+  const password = loginForm.password.value
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(cred => {
+      console.log('user logged in:', cred.user)
+      loginForm.reset()
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// subscribing to auth changes
+const unsubAuth = onAuthStateChanged(auth, (user) => {
+    console.log('user status changed:', user)
+  })
+  
+  // unsubscribing from changes (auth & db)
+  const unsubButton = document.querySelector('.unsub')
+  unsubButton.addEventListener('click', () => {
+    console.log('unsubscribing')
+    unsubCol()
+    unsubDoc()
+    unsubAuth()
   })
